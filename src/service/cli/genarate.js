319e83bd2d4116be165208de09d3b55154ed3882
Konstantin Surikov <surikov@gmail.com>
2020-02-11 01:27:11 +0300

    Личный проект: покоритель файлов

--- src/service/cli/genarate.js
+++ src/service/cli/genarate.js
@@ -1,105 +1,69 @@
 'use strict';
 
 const chalk = require(`chalk`);
-
 const fs = require(`fs`).promises;
 
 const {getRandomInt, shuffle} = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 const MAX_ANNOUNCES = 5;
 const MAX_COUNT = 1000;
 const MAX_CATEGORIES = 3;
 const TIME_SPAN = 91 * 24 * 60 * 60 * 1000;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
-
 const getRandomFromArray = (array) => {
   return array[getRandomInt(0, array.length - 1)];
 };
 
-const getPublications = (count) => {
+const getPublications = (count, sentences, titles, categories) => {
   return Array(count)
     .fill({})
     .map((it, i) => {
       return {
         id: i,
-        title: getRandomFromArray(TITLES),
+        title: getRandomFromArray(titles),
         createdDate: new Date(getRandomInt(Date.now() - TIME_SPAN, Date.now()))
           .toLocaleString(),
-        announce: shuffle(SENTENCES)
+        announce: shuffle(sentences)
           .slice(0, getRandomInt(0, MAX_ANNOUNCES)),
-        fullText: shuffle(SENTENCES)
-          .slice(0, getRandomInt(0, SENTENCES.length - 1)),
-        сategory: shuffle(CATEGORIES)
+        fullText: shuffle(sentences)
+          .slice(0, getRandomInt(0, sentences.length - 1)),
+        сategory: shuffle(categories)
           .slice(0, getRandomInt(1, MAX_CATEGORIES)),
       };
     });
+};
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
 };
 
 module.exports = {
   name: `--generate`,
 
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
     const [count] = args;
     const publicationsCount = parseInt(count, 10) || DEFAULT_COUNT;
 
     if (publicationsCount < MAX_COUNT) {
-      const content = JSON.stringify(getPublications(publicationsCount));
-      console.log(content);
+      const content = JSON.stringify(getPublications(publicationsCount, sentences, titles, categories));
       try {
         await fs.writeFile(FILE_NAME, content);
-        return console.info(chalk.green(`Operation succeeded, file created.`));
-      } catch {
+        console.info(chalk.green(`Operation succeeded, file created.`));
+      } catch (err) {
         console.error(chalk.error(`Can't write data to file`));
         process.exit(1);
       }
@@ -109,5 +73,3 @@ module.exports = {
     }
   }
 };
-
-
