d49705b0021085d13c6dcd26234baa46daf98795
Konstantin Surikov <surikov@gmail.com>
2021-11-08 17:08:03 +0300

    Added the logging

--- src/constants.js
+++ src/constants.js
@@ -19,3 +19,7 @@ module.exports.ExitCode = {
 };
 module.exports.MAX_ID_LENGTH = 6;
 module.exports.API_PREFIX = `/api`;
+module.exports.Env = {
+  DEVELOPMENT: `development`,
+  PRODUCTION: `production`
+};
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,36 +1,54 @@
 'use strict';
 
-const chalk = require(`chalk`);
 const express = require(`express`);
+const routes = require(`../api`);
+const {getLogger} = require(`../lib/logger`);
 
 const {HttpCode, API_PREFIX, ExitCode} = require(`../../constants`);
-const routes = require(`../api`);
-
 const DEFAULT_PORT = 3000;
 
+
 const app = express();
+const logger = getLogger({name: `api`});
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  logger.debug(`A request on the route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`The response's status code is ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, routes);
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+app.use((req, res) => {
+  res
+    .status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`The route ${req.url} wasn't found`);
+});
 
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred while processing the request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = parseInt(customPort, 10) || DEFAULT_PORT;
-
-
-    app.listen(port, (err) => {
-      if (err) {
-        return console.log(chalk.red(`Ошибка создания сервера`), err);
-      }
-      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-    });
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return logger.error(`An error occurrued on server creation: ${err.message()}`);
+        }
+        return logger.info(`Listening to connections on port ${port}`);
+      });
+    } catch (err) {
+      logger.error(`An error occurred : ${err.message()}`);
+      process.exit(ExitCode.error);
+    }
   }
 };
